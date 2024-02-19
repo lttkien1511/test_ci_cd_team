945bfd780d721824abe01ffc60943a833b488f59
lttkien1511 <kiendeclan1511@gmail.com>
2024-02-19 14:22:18 +0700

    change app test js

--- reactjs_project/src/App.test.js
+++ reactjs_project/src/App.test.js
@@ -1,19 +1,21 @@
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent} from '@testing-library/react';
 import App from './App';
 import React from 'react';
-import Homepage from './pages/homepage';
+// import Homepage from './pages/homepage';
+import { getdata } from './model/homepage';
 
 
 test('submit the form and display results', async () => {
-  // const {getByPlaceholderText, getByRole} = render(<App />);
-  render(<App/>);
-  const input = screen.getByPlaceholderText('Enter laptop name');
+  const {getByPlaceholderText, getByRole} = render(<App />);
+  // render(<App/>);
+  const input = getByPlaceholderText('Enter laptop name');
   fireEvent.change(input, {target: {value: '...'}});
 
-  const button = screen.getByRole('button', {name: 'Submit'});
+  const button = getByRole('button', {name: 'Submit'});
   fireEvent.click(button);
 
-  const resultElement = await screen.findByTestId('results');
-  expect(resultElement).toBeInTheDocument();
+  const results = await getdata();
+  expect(results.length).toBeGreaterThan(0);
+
 });
 
