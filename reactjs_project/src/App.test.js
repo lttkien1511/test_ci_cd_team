060eb27d8003e1f93f166740dc51f359971d203f
lttkien1511 <kiendeclan1511@gmail.com>
2024-02-19 09:24:35 +0700

    change app test js

--- reactjs_project/src/App.test.js
+++ reactjs_project/src/App.test.js
@@ -2,12 +2,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-test('should render without crashing', () => {
-  render(<App/>);
+test('renders learn react link', () => {
+  const {getByText} = render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
+
+// test('should render without crashing', () => {
+//   render(<App/>);
+// });
